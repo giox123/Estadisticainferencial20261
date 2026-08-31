71ef2f4fd143c66a5929cae2f15094a32fc0c069
giox123 <jovanytorre.1@gmail.com>
2026-08-31 13:53:26 -0600

    Create cargar_datos.py

---
+++ Unidad1/codigos/cargar_datos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+link_csv = "https://raw.githubusercontent.com/giox123/Estadisticainferencial20261/refs/heads/main/Unidad1/problemario/dato_problema_01.csv"
+
+df = pd.read_csv(link_csv)
+
+df
